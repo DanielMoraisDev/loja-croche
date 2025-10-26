85dc9f6c2d794f328c1f5c06fe54065631dd2af8
DanielMoraisDev <danielfelipe2905@gmail.com>
2025-10-26 09:24:09 -0300

    refactor(image-upload): changed to upload for server

--- backend/src/middleware/image-upload.js
+++ backend/src/middleware/image-upload.js
@@ -1,44 +1,15 @@
 import express from "express";
 import multer from "multer";
-import fs from "fs";
 
 const app = express();
 
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-
-const __fileName = fileURLToPath(import.meta.url);
-const __dirName = path.dirname(__fileName);
-
-const imageStore = multer.diskStorage({
-  destination: (req, file, cb) => {
-    let folder = "";
-
-    if (req.baseUrl.includes("products")) {
-      folder = "products";
-    }
-
-    const uploadPath = path.join(__dirName, `../../uploads/${folder}`);
-
-    if (!fs.existsSync(uploadPath)) {
-      fs.mkdirSync(uploadPath, { recursive: true });
-    }
-
-    cb(null, uploadPath);
-  },
-  filename: (req, file, cb) => {
-    cb(
-      null,
-      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-    );
-  },
-});
+const storage = multer.memoryStorage();
 
 export const imageUpload = multer({
-  storage: imageStore,
-  limits: { fileSize: "500000" },
+  storage,
+  limits: { fileSize: 50 * 1024 * 1024 },
   fileFilter(req, file, cb) {
-    if (!file.originalname.match(/\.(png||jpg||webp)$/)) {
+    if (!file.originalname.match(/\.(png|jpg|jpeg|webp)$/i)) {
       return cb(
         new Error("Por favor, envie apenas arquivos: JPG, PNG ou WEBP")
       );
